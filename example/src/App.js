9ef90ac5c50a965cebf628af9776d7c847530838
Rick Brown <ricky.brown.00@gmail.com>
2019-12-05 16:45:42 +0000

    working version

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import { useMyHook } from '@rickbrown/use-local-storage'
+import { useLocalStorage } from '@rickbrown/use-local-storage'
 
+// An basis example which has an input field and a button.
+// Typing into the text box will automatically save into local-storage.
+// So typing your name and hitting refresh, will persist your name.
+// Clicking the button will overwrite the local-storage value with 'updated'.
 const App = () => {
-  const example = useMyHook()
+  // Array destructuring from our custom hook.
+  // It accepts 2 arguments for its initial value. ('key','value')
+
+  const [name, setName] = useLocalStorage('name', 'Rick')
+
   return (
     <div>
-      {example}
+      <h2>Local Storage custom React hook</h2>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={() => setName('updated')}>Click</button>
     </div>
   )
 }
+
 export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 import * as React from 'react'
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
+export function useLocalStorage(key, initialValue) {
+  // Setup for the storedValue that we return in our hook API
+  // Implement a function to useState which will use either, the value which ...
+  // ... is currently stored in local-storage. Or if there is no current ...
+  // ... value then it will return the initial value. This function will only be ...
+  // ... called on the first render, useState will manage it after that.
+  // A try/catch block is used in this function to handle any errors.
+  const [storedValue, setStoredValue] = React.useState(() => {
+    try {
+      // Return the value currently in local storage or initial state.
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (err) {
+      // If error just return the initial value.
+      console.log(err)
+      return initialValue
+    }
   })
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
+  // Setup for the setValueInLocalStorage function that we return in our hook API
+  const setValueInLocalStorage = value => {
+    // Use a try/catch block to handle any errors.
+    try {
+      // Replicate the update pattern which useState uses.
+      // eg: setFunction(prev => prev)
+      const updatedArgValue =
+        value instanceof Function ? value(storedValue) : value
+      // Set our useState hook to the updated value.
+      setStoredValue(updatedArgValue)
+      // Save the updated value in local-storage.
+      window.localStorage.setItem(key, JSON.stringify(updatedArgValue))
+    } catch (err) {
+      // A better error handling implementation is needed.
+      console.log(err)
     }
-  }, [])
-
-  return counter
+  }
+  return [storedValue, setValueInLocalStorage]
 }
